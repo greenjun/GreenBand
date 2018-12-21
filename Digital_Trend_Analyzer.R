7af5f43113cae8485f316da15a3555186c55a94c
Yun Junhyuck <junhyuck147@gmail.com>
2018-12-21 13:09:17 +0900

    commit

--- Digital_Trend_Analyzer.R
+++ Digital_Trend_Analyzer.R
@@ -10,13 +10,13 @@
 
 library(tidyverse) # Preprocessing, Visualizing
 
+library(lubridate) # time data
+
 library(knitr) # Tidy document
 
 library(ggrepel) # ggplot tidy text
 
 setwd("C:/Project/GreenBand_Lpoint5/Source/") ; getwd()
-
-load(file="Code/R/Digital_Trend_Analyzer.RData")
 
 #=====================================================================================================================
 # Step 1 : 분석 데이터 호출
@@ -24,81 +24,209 @@ load(file="Code/R/Digital_Trend_Analyzer.RData")
 
 ## Search1 data
 
-#Search1_original <- read.csv("Data/Search1_original.csv", header = T, encoding = "UTF-8")
+Search1_original <- read.csv("Data/Search1_original.csv", header = T, encoding = "UTF-8")
 
-#Search1_original[is.na(Search1_original$SEARCHCNT),5] <- Search1_original[!is.na(Search1_original$X),4]
+Search1_original[is.na(Search1_original$SEARCHCNT),5] <- Search1_original[!is.na(Search1_original$X),4]
 
-#Search1_original <- Search1_original[,-4]
+Search1_original <- Search1_original[,-4]
 
-#colnames(Search1_original) <- c("CLNT_ID", "SESS_ID", "KWD_NM", "SEARCH_CNT")
+colnames(Search1_original) <- c("CLNT_ID", "SESS_ID", "KWD_NM", "SEARCH_CNT")
 
-#Search1_original$KWD_NM <- gsub(pattern = ",", replacement = "", Search1_original$KWD_NM)
+Search1_original$KWD_NM <- gsub(pattern = ",", replacement = "", Search1_original$KWD_NM)
 
 ## Session data
 
-#Session_original <- read.csv("Data/Session_original.csv", stringsAsFactors = F, header = T, encoding = "UTF-8")
+Session_original <- read.csv("Data/Session_original.csv", stringsAsFactors = F, header = T, encoding = "UTF-8")
 
-#colnames(Session_original) <- c("CLNT_ID", "SESS_ID", "SESS_SEQ", "SESS_DT", "TOT_PAG_VIEW_CT", "TOT_SESS_HR_V", "DVC_CTG_NM", "ZON_NM", "CITY_NM")
+colnames(Session_original) <- c("CLNT_ID", "SESS_ID", "SESS_SEQ", "SESS_DT", "TOT_PAG_VIEW_CT", "TOT_SESS_HR_V", "DVC_CTG_NM", "ZON_NM", "CITY_NM")
 
-#sapply(Session_original, function(x){sum(is.na(x))})
+sapply(Session_original, function(x){sum(is.na(x))})
 
-#which(Session_original$TOT_SESS_HR_V == "")
+which(Session_original$TOT_SESS_HR_V == "")
 
 ## Custom data
 
-#Custom_original <- read.csv("Data/Custom_original.csv", stringsAsFactors = F, header = T, encoding = "UTF-8")
+Custom_original <- read.csv("Data/Custom_original.csv", stringsAsFactors = F, header = T, encoding = "UTF-8")
 
-#colnames(Custom_original) <- c("CLNT_ID", "CLNT_GENDER", "CLNT_AGE" )
+colnames(Custom_original) <- c("CLNT_ID", "CLNT_GENDER", "CLNT_AGE" )
 
 ## Master data - due to special character, moved data to python
 
-#Master_original <- read.csv("Data/python_Master_original.csv", stringsAsFactors = F, header = T)
+Master_original <- read.csv("Data/python_Master_original.csv", stringsAsFactors = F, header = T)
 
-#Master_original <- Master_original[c(1:847652),]
+Master_original <- Master_original[c(1:847652),]
 
 ## Product data
 
-#Product_original <- read.csv("Data/python_Product_original.csv", stringsAsFactors = F, header = T)
+Product_original <- read.csv("Data/python_Product_original.csv", stringsAsFactors = F, header = T)
 
 ## Search2
 
-#Search2_original <- read.csv("Data/python_Search2_original.csv", stringsAsFactors = F, header = T)
+Search2_original <- read.csv("Data/python_Search2_original.csv", stringsAsFactors = F, header = T)
 
 ## save RData
 
-# save.image("C:/Users/YunJunHyuck/Desktop/lpoint/code/R/online_preference_index_data.RData")
+save.image("C:/Project/GreenBand_Lpoint5/Source/Code/R/online_preference_index_data.RData")
 
 ## load RData
 
-## load(file="code/R/Online_preference_index_data.RData")
+load(file="Code/R/Digital_Trend_Analyzer.RData")
 
 #=====================================================================================================================
 # Step 2 : 데이터 전처리
 #=====================================================================================================================
 
+summary(Product_original) ; str(Product_original) ; View(Product_original) ; sapply(Product_original, function(x){sum(is.na(x))})
+
+summary(Search1_original) ; str(Search1_original) ; View(Search1_original) ; sapply(Search1_original, function(x){sum(is.na(x))})
+
+summary(Search2_original) ; str(Search1_original) ; View(Search2_original) ; sapply(Search1_original, function(x){sum(is.na(x))})
+
+summary(Custom_original) ; str(Custom_original) ; View(Custom_original) ; sapply(Custom_original, function(x){sum(is.na(x))})
+
+summary(Session_original) ; str(Session_original) ; View(Session_original) ; sapply(Session_original, function(x){sum(is.na(x))})
+
+summary(Master_original) ; str(Master_original) ; View(Master_original) ; sapply(Master_original, function(x){sum(is.na(x))})
+
+## Product data
+
+tr_Product_original <- Product_original %>% 
+  mutate(PD_BUY_AM = as.numeric(gsub(",","", PD_BUY_AM)),
+         PD_BUY_CT = as.numeric(gsub(",","", PD_BUY_CT)))
+
+# Product data$PD_ADD_NM -> 추후 처리
+
+## Search1 data
+
+tr_Search1_original <- Search1_original
+
+## Search2 data
+
+tr_Search2_original <- Search2_original %>%
+  mutate(SESS_DT = ymd(SESS_DT),
+         SEARCH_CNT = as.numeric(gsub(",","", SEARCH_CNT)))
+
+## Custom data
+
+tr_Custom_original <- Custom_original %>%
+  mutate(CLNT_AGE = as.factor(CLNT_AGE))
+
+## Session data
+
+Session_original$TOT_SESS_HR_V[Session_original$TOT_SESS_HR_V == ""] <- NA
+
+tr_Session_original <- Session_original %>%
+  mutate(SESS_DT = ymd(SESS_DT),
+         TOT_SESS_HR_V = as.numeric(gsub(",","", TOT_SESS_HR_V)),
+         DVC_CTG_NM = as.factor(DVC_CTG_NM),
+         ZON_NM = as.factor(ZON_NM),
+         CITY_NM = as.factor(CITY_NM))
+
+## Master data
+
+tr_Master_original <- Master_original[1:847652,]
 
 
+## Save tr_data
 
+rm(list = c('Custom_original','Master_original','Product_original','Search1_original','Search2_original','Session_original'))
 
+## save RData
 
+save.image("C:/Project/GreenBand_Lpoint5/Source/Code/R/tr_Digital_Trend_Analyzer.RData")
 
+## load RData
 
-
-
+load(file="Code/R/tr_Digital_Trend_Analyzer.RData")
 
 #=====================================================================================================================
 # Step 3 : 탐색적 데이터 분석 
 #=====================================================================================================================
 
-# one by one data analysis
 
-summary(Custom_original)
 
-summary(Master_original)
 
-summary(Product_original)
 
-summary(Session_original) ; View(Session_original)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# HITS_SEQ는 상품 구매가 이루어지기 전 까지 Session 내 활동들이 얼마나 되는지 확인
+
+qplot(y = Product_original$HITS_SEQ, geom=c('boxplot'))
+
+# median 49번 정도의 세션 내 클릭하게 된다.
+
+
+## Product_original data + Search1_original data
+
+Product_Search1_original <- full_join(Product_original, Search1_original, c('CLNT_ID', 'SESS_ID')) ; View(Product_Search1_original)
+
+summary(Product_Search1_original) ; str(Product_Search1_original) ; View(Product_Search1_original)
+
+sapply(Product_Search1_original, function(x){sum(is.na(x))})
+
+# Product의 NA는 구매한 방문자인데 검색을 했지만, 구매를 안헀다?? 모순.. 뭐지  ?? 
+
+# Search1의  NA는 검색하지 않고 바로구입한 사람 
+
+##############################
+
+## Search2_original data
+
+# 데이터 부피가 너무 커서 샘플로 확인
+
+
+
+# 날짜 데이터 처리, 숫자로 전환 
+
+## 총 검색량 흐름
+View(
+  Search2_original %>%
+    group_by(SESS_DT) %>%
+    mutate(count = sum(SEARCH_CNT))
+  )
+
+# 같은 날짜의 검색어 통합
+
+# 검색어의 분류 - 상품군
+
+# 검색량을 바탕으로 지수에 포함
+
+# 주의할 것은 한 사람이 여러번 검색한 것은 어떻게 처리? 
+
+##############################
+
+
+summary(Custom_original) ; str(Search2_original) ; View(Custom_original)
+
+
+
 
 View(Session_original %>%
   arrange(SESS_SEQ, SESS_DT))
--- Digital_Trend_Analyzer_report.md
+++ Digital_Trend_Analyzer_report.md
@@ -13,34 +13,81 @@ output:
 ---
 
 
-
-
-
-
-
-
-
-
 # Step 0 : 분석 환경 설정
 
 
+```
+## [1] "C:/Project/GreenBand_Lpoint5/Source"
+```
 
 
 ```r
-library(dplyr)
+library(tidyverse) # Preprocessing, Visualizing
 
-library(ggplot2)
+library(knitr) # Tidy document
 
-library(ggrepel)
-
-library(readr)
-
-library(stringr)
+library(ggrepel) # ggplot tidy text
 ```
 
 # Step 1 : data pre-processing
 
+
 # Step 2 : EDA
+
+
+```r
+summary(Product_original)
+```
+
+```
+##     CLNT_ID           SESS_ID            HITS_SEQ           PD_C       
+##  Min.   :     25   Min.   :      26   Min.   :  1.00   Min.   :     4  
+##  1st Qu.:1656017   1st Qu.: 3242700   1st Qu.: 24.00   1st Qu.:278909  
+##  Median :3337207   Median : 6019554   Median : 49.00   Median :513734  
+##  Mean   :3323788   Mean   : 5828583   Mean   : 79.52   Mean   :488052  
+##  3rd Qu.:4994410   3rd Qu.: 8428752   3rd Qu.:102.00   3rd Qu.:718836  
+##  Max.   :6592554   Max.   :10977899   Max.   :500.00   Max.   :847652  
+##   PD_ADD_NM          PD_BRA_NM          PD_BUY_AM        
+##  Length:1048575     Length:1048575     Length:1048575    
+##  Class :character   Class :character   Class :character  
+##  Mode  :character   Mode  :character   Mode  :character  
+##                                                          
+##                                                          
+##                                                          
+##   PD_BUY_CT        
+##  Length:1048575    
+##  Class :character  
+##  Mode  :character  
+##                    
+##                    
+## 
+```
+
+```r
+kable(head(Product_original), caption = 'Table1', align = 'c')
+```
+
+
+
+Table: Table1
+
+ CLNT_ID    SESS_ID     HITS_SEQ     PD_C                   PD_ADD_NM                     PD_BRA_NM      PD_BUY_AM    PD_BUY_CT 
+---------  ----------  ----------  --------  ----------------------------------------  ---------------  -----------  -----------
+ 4139680    7605037        12       642112                색상:워터멜론                 [바비브라운]      39,000          1     
+ 4140076    10189797       13       570603    색상:BLK0_(BLK0)BLACK|사이즈:120 / 2개       데상트         39,000          2     
+ 4142395    6158159        85       179538                  (not set)                     [아베다]        39,000          1     
+ 4144914    7935714        12       554336    색상:블랙|사이즈:160cm(12~13세) / 1개     아디다스 키즈     39,000          1     
+ 4144917    6406509        78       190306                     5개                        데코르테        39,000          5     
+ 4147643    4050743        66       149995         사이즈:옵션04.AN868_04 / 1개         앵글로매니아      39,000          1     
+
++ PD_ADD_NM에 중복되는 내용이 존재  
++ HITS_SEQ 가 1부터 시작한다고 했는데 1부터 시작하는게 확인 불가 
+
+
+
+
+
+
 
 # Step 3 : Modeling
 
@@ -50,11 +97,35 @@ library(stringr)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ```r
 plot(iris$Sepal.Length,iris$Petal.Length)
 ```
 
-![Image1](Digital_Trend_Analyzer_report_files/figure-html/unnamed-chunk-3-1.png)
+![Image1](Digital_Trend_Analyzer_report_files/figure-html/unnamed-chunk-5-1.png)
 
 
 ```r
